73cac23ab7f0fe8dec7be02e0a5d9fea0bc63ed5
nsanchez9009 <nsanchez9009@gmail.com>
2024-11-01 15:46:41 -0600

    updated middleware

--- api/middleware/authMiddleware.js
+++ api/middleware/authMiddleware.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 const verifyToken = (req, res, next) => {
-  // Check for token in cookies or Authorization header
-  const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+  const token = req.cookies?.token || (req.headers.authorization && req.headers.authorization.split(' ')[1]);
+
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
